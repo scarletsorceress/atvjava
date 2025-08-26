2f6e9a0f498fa806325af301ddb8cfdceeba3a72
Bryan Ferreira Barboza <83647639+scarletsorceress@users.noreply.github.com>
2025-08-26 10:08:59 -0300

    Add files via upload

---
+++ atv1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class atv1{
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+
+        float[] notas = new float[3];
+        float soma = 0;
+
+        for(int i = 0; i < 3; i++) {
+            System.out.printf("Manda sua nota %d:%n", i + 1);
+            notas[i] = scan.nextFloat();
+            soma += notas[i];
+        }
+
+        float media = soma / 3;
+        System.out.printf("Sua nota final é %f\n", media);
+
+        if(media >= 5 && media < 7){
+            System.out.println("Recuperação");
+        } else if(media >= 7){
+            System.out.println("Aprovado");
+        } else if(media < 5){
+            System.out.println("Reprovado");
+        }
+    }
+}
---
+++ atv2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class atv2 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Diga o número que você quer a tabuada: ");
+        int numero = scan.nextInt();
+
+        for(int i = 0; i <= 10; i++){
+            int resultado = numero*i;
+            System.out.printf("%d x %d = %d\n", numero, i, resultado);
+        }
+    }
+}
